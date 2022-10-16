d02cdaef243170ba587bcce9c0228eef17b8d999
Shoeman <16366343+Shoeman@users.noreply.github.com>
2022-10-16 17:31:54 +0100

    Add sample variance and standard deviation

--- src/main/scala/com/ScalaAndStats/Variance.scala
+++ src/main/scala/com/ScalaAndStats/Variance.scala
@@ -17,8 +17,27 @@ object Variance {
     A.div(sumDiffsSquared, A.fromInt(values.size))
   }
 
+  // TODO refactor methods together
+  def sampleVariance[A](values: Seq[A])(implicit A: Fractional[A]): A = {
+
+    if (values.length <= 1) {
+      return A.fromInt(0)
+    }
+    val mean = Averages.mean(values)
+    val meanDiff: A => A = value => value - mean
+    val sumDiffsSquared = values.map(meanDiff).map(a => a * a).sum
+
+    A.div(sumDiffsSquared, A.fromInt(values.size - 1))
+  }
+
   def populationStandardDeviation[A](values: Seq[A])(implicit A: Fractional[A]): Double = {
     val variance = populationVariance(values)
+    sqrt(variance.toDouble)
+  }
+
+  // TODO refactor methods together
+  def sampleStandardDeviation[A](values: Seq[A])(implicit A: Fractional[A]): Double = {
+    val variance = sampleVariance(values)
     sqrt(variance.toDouble)
   }
 }
--- src/test/scala/com/ScalaAndStats/VarianceTest.scala
+++ src/test/scala/com/ScalaAndStats/VarianceTest.scala
@@ -1,6 +1,6 @@
 package com.ScalaAndStats
 
-import com.ScalaAndStats.Variance.{populationStandardDeviation, populationVariance}
+import com.ScalaAndStats.Variance.{populationStandardDeviation, populationVariance, sampleStandardDeviation, sampleVariance}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -35,6 +35,32 @@ class VarianceTest extends AnyFlatSpec with Matchers {
     populationVariance(values) should equal(2.917 +- TOLERANCE)
   }
 
+  "sampleVariance" should "handle empty sequence" in {
+    val values: Seq[Double] = Seq()
+    sampleVariance(values) should equal(0)
+  }
+
+  // Not sure what should really happen in this case
+  "sampleVariance" should "handle single item" in {
+    val values: Seq[Double] = Seq(5)
+    sampleVariance(values) should equal(0)
+  }
+
+  "sampleVariance" should "handle zero variance" in {
+    val values: Seq[Double] = Seq(2, 2, 2, 2)
+    sampleVariance(values) should equal(0)
+  }
+
+  "sampleVariance" should "handle simple variance" in {
+    val values: Seq[Double] = Seq(1, 1, 1, 5, 5, 5)
+    sampleVariance(values) should equal(4.8 +- TOLERANCE)
+  }
+
+  "sampleVariance" should "handle complex variance" in {
+    val values: Seq[Double] = Seq(1, 2, 3, 4, 5, 6)
+    sampleVariance(values) should equal(3.5 +- TOLERANCE)
+  }
+
   "populationStandardDeviation" should "handle empty sequence" in {
     val values: Seq[Double] = Seq()
     populationStandardDeviation(values) should equal(0)
@@ -58,5 +84,30 @@ class VarianceTest extends AnyFlatSpec with Matchers {
   "populationStandardDeviation" should "handle floating point variance" in {
     val values: Seq[Double] = Seq(1, 2, 3, 4, 5, 6)
     populationStandardDeviation(values) should equal(1.707 +- TOLERANCE)
+  }
+
+  "sampleStandardDeviation" should "handle empty sequence" in {
+    val values: Seq[Double] = Seq()
+    sampleStandardDeviation(values) should equal(0)
+  }
+
+  "sampleStandardDeviation" should "handle single item" in {
+    val values: Seq[Double] = Seq(1)
+    sampleStandardDeviation(values) should equal(0)
+  }
+
+  "sampleStandardDeviation" should "handle zero variance" in {
+    val values: Seq[Double] = Seq(2, 2, 2, 2)
+    sampleStandardDeviation(values) should equal(0)
+  }
+
+  "sampleStandardDeviation" should "handle simple variance" in {
+    val values: Seq[Double] = Seq(1, 1, 1, 5, 5, 5)
+    sampleStandardDeviation(values) should equal(2.191 +- TOLERANCE)
+  }
+
+  "sampleStandardDeviation" should "handle floating point variance" in {
+    val values: Seq[Double] = Seq(1, 2, 3, 4, 5, 6)
+    sampleStandardDeviation(values) should equal(1.871 +- TOLERANCE)
   }
 }
